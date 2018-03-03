7b8e0cc4afd2073c08627b731ca944c35e82662e
christian <christiandavidphoto@gmail.com>
2018-03-02 21:20:33 -0500

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+// var React = require('react');
+import React from 'react';
+import Popular from './Popular';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
+  }
+}
+
+// module.exports = App;
+export default App;
---
+++ app/index.js
@@ -0,0 +1,11 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var App = require('./components/App');
+// require('./index.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('app'));
