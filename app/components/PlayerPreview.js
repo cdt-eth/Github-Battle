102ff67f1aff5f0d9dbd167e53f4aff6c1bdfe82
christian <christiandavidphoto@gmail.com>
2018-04-13 16:50:12 -0400

    style/alt-tag-color-fix

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -5,7 +5,7 @@ function PlayerPreview(props) {
   return (
     <div>
       <div className="column">
-        <img className="avatar" src={props.avatar} alt={'Avatar for ' + props.username} />
+        <img className="avatar" src={props.avatar} alt={'Avatar for ' + props.username} style={{ color: 'white' }} />
         <h2 className="username">@{props.username}</h2>
       </div>
       {props.children}
