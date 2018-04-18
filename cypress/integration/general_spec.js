1773d9b05fb25dd087f97e411da6f746ca1e3621
christian <christiandavidphoto@gmail.com>
2018-04-18 10:02:44 -0400

    refactor/cypress-test-updates

--- cypress/integration/general_spec.js
+++ cypress/integration/general_spec.js
@@ -3,64 +3,91 @@ describe('Github Battle', function() {
     cy.visit('http://localhost:8080');
   });
 
-  // Check all nav pages
-  it('Clicks Battle in nav', function() {
+  // ___________________________________________
+  // ______________CHECK NAV PAGES______________
+  // ___________________________________________
+  it('Clicks Battle in nav & visits page', function() {
     cy
       .get('.nav')
       .contains('Battle')
       .click();
+    cy.url().should('eq', 'http://localhost:8080/battle');
   });
-  it('Clicks Popular in nav', function() {
+
+  it('Clicks Popular in nav & visits page', function() {
     cy
       .get('.nav')
       .contains('Popular')
       .click();
+    cy.url().should('eq', 'http://localhost:8080/popular');
   });
-  it('Clicks Home in nav', function() {
+
+  it('Clicks Home in nav & visits page', function() {
     cy
       .get('.nav')
       .contains('Home')
       .click();
+    cy.url().should('eq', 'http://localhost:8080/');
   });
 
-  // Move into Battle sequence
-  it('Clicks homepage battle button', function() {
+  it('Visits non-existent page', function() {
+    cy.visit('http://localhost:8080/home');
+  });
+
+  it('Clicks Home in nav & visits page', function() {
+    cy
+      .get('.nav')
+      .contains('Home')
+      .click();
+    cy.url().should('eq', 'http://localhost:8080/');
+  });
+
+  // _______________________________________
+  // ____________BATTLE SEQUENCE____________
+  // _______________________________________
+  it('Clicks homepage battle button & visits page', function() {
     cy
       .get('.homeButton')
       .contains('Battle')
       .click();
-  });
-  it('Visits Battle page', function() {
-    cy.url('http://localhost:8080/battle');
+    cy.url().should('eq', 'http://localhost:8080/battle');
   });
 
-  // Load both users unsuccessfully
+  // ________________________________________
+  // _____LOAD BOTH USERS UNSUCCESSFULLY_____
+  // ________________________________________
   it('Inputs user 1', function() {
     cy
       .get('input:first')
       .type('reallyfakeusername')
       .should('have.value', 'reallyfakeusername');
   });
+
   it('Submits user 1', function() {
     cy.get(':nth-child(1) > .button').click();
   });
+
   it('Inputs user 2', function() {
     cy
       .get('#username')
       .type('anotherfakeusername')
       .should('have.value', 'anotherfakeusername');
   });
+
   it('Submits user 2', function() {
     cy.get(':nth-child(2) > .button').click();
   });
-  // Initiate battle once both users are present
+
   it('Clicks battle button', function() {
     cy
       .get('.button')
       .contains('Battle')
       .click();
   });
-  // Reset Battle
+
+  // ________________________________________
+  // ______________RESET BATTLE______________
+  // ________________________________________
   it('Clicks reset button', function() {
     cy
       .get('.button')
@@ -68,33 +95,41 @@ describe('Github Battle', function() {
       .click();
   });
 
-  // Load left user successfully & right user unsuccessfully
+  // ___________________________________________________
+  // ___LOAD LEFT SUCCESSFULLY & RIGHT UNSUCCESSFULLY___
+  // ___________________________________________________
   it('Inputs user 1', function() {
     cy
       .get('input:first')
       .type('christiandavidturner')
       .should('have.value', 'christiandavidturner');
   });
+
   it('Submits user 1', function() {
     cy.get(':nth-child(1) > .button').click();
   });
+
   it('Inputs user 2', function() {
     cy
       .get('#username')
       .type('reallyfakeusername')
       .should('have.value', 'reallyfakeusername');
   });
+
   it('Submits user 2', function() {
     cy.get(':nth-child(2) > .button').click();
   });
-  // Initiate battle once both users are present
+
   it('Clicks battle button', function() {
     cy
       .get('.button')
       .contains('Battle')
       .click();
   });
-  // Reset Battle
+
+  // ________________________________________
+  // ______________RESET BATTLE______________
+  // ________________________________________
   it('Clicks reset button', function() {
     cy
       .get('.button')
@@ -102,33 +137,41 @@ describe('Github Battle', function() {
       .click();
   });
 
-  // Load left user unsuccessfully & right user successfully
+  // ___________________________________________________
+  // ___LOAD LEFT UNSUCCESSFULLY & RIGHT SUCCESSFULLY___
+  // ___________________________________________________
   it('Inputs user 1', function() {
     cy
       .get('input:first')
       .type('reallyfakeusername')
       .should('have.value', 'reallyfakeusername');
   });
+
   it('Submits user 1', function() {
     cy.get(':nth-child(1) > .button').click();
   });
+
   it('Inputs user 2', function() {
     cy
       .get('#username')
       .type('christiandavidturner')
       .should('have.value', 'christiandavidturner');
   });
+
   it('Submits user 2', function() {
     cy.get(':nth-child(2) > .button').click();
   });
-  // Initiate battle once both users are present
+
   it('Clicks battle button', function() {
     cy
       .get('.button')
       .contains('Battle')
       .click();
   });
-  // Reset Battle
+
+  // ________________________________________
+  // ______________RESET BATTLE______________
+  // ________________________________________
   it('Clicks reset button', function() {
     cy
       .get('.button')
@@ -136,26 +179,31 @@ describe('Github Battle', function() {
       .click();
   });
 
-  // Load both users successfully
+  // ____________________________________
+  // ____LOAD BOTH USERS SUCCESSFULLY____
+  // ____________________________________
   it('Inputs user 1', function() {
     cy
       .get('input:first')
       .type('christiandavidturner')
       .should('have.value', 'christiandavidturner');
   });
+
   it('Submits user 1', function() {
     cy.get(':nth-child(1) > .button').click();
   });
+
   it('Inputs user 2', function() {
     cy
       .get('#username')
       .type('torchcodelab')
       .should('have.value', 'torchcodelab');
   });
+
   it('Submits user 2', function() {
     cy.get(':nth-child(2) > .button').click();
   });
-  // Initiate battle once both users are present
+
   it('Clicks battle button', function() {
     cy
       .get('.button')
@@ -163,32 +211,52 @@ describe('Github Battle', function() {
       .click();
   });
 
-  // Back to Popular page to check all languages
-  it('Visits Popular page', function() {
-    cy.get(':nth-child(3) > a').click();
+  // _____________________________
+  // _____STARTS A NEW BATTLE_____
+  // _____________________________
+  it('Clicks battle again button', function() {
+    cy
+      .get('.battleAgain')
+      .contains('Battle again')
+      .click();
   });
+
+  // _____________________________________________
+  // _______CHECK LANGUAGES ON POPULAR PAGE_______
+  // _____________________________________________
+  it('Visits Popular page', function() {
+    cy
+      .get('.nav')
+      .contains('Popular')
+      .click();
+    cy.url().should('eq', 'http://localhost:8080/popular');
+  });
+
   it('Runs through all languages', function() {
-    cy // Javascript
+    cy
       .get('.languages > :nth-child(2)')
+      .contains('Javascript')
       .wait(2000)
       .click();
-    cy // Ruby
+    cy
       .get('.languages > :nth-child(3)')
+      .contains('Ruby')
       .wait(2000)
       .click();
-    cy // Java
+    cy
       .get('.languages > :nth-child(4)')
+      .contains('Java')
       .wait(2000)
       .click();
-    cy // CSS
+    cy
       .get('.languages > :nth-child(5)')
+      .contains('CSS')
       .wait(2000)
       .click();
-    cy // Python
+    cy
       .get('.languages > :nth-child(6)')
+      .contains('Python')
       .wait(2000)
       .click();
-    //   ________test 1st Python repo________
-    //     cy.get(':nth-child(1) > .space-list-items > :nth-child(2) > a > .linkSpan').click();
   });
 });
